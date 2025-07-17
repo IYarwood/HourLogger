2efac31ad31be66391d765633b56e18421cce2eb
IYarwood <ianzyarwood@gmail.com>
2025-07-17 17:30:21 -0400

    settings from env

--- HourLoggerProject/HourLoggerProject/settings.py
+++ HourLoggerProject/HourLoggerProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kx!(ikvxbre+66r0*^ing7y0nnoajzbbw9wn@_5=2p@&lmj35^'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['31.97.100.20', "localhost", '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 
 # Application definition
